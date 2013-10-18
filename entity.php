25a7137856e76650cca69bed5b36593889504ef3
于彤 <luckyyutong@gmail.com>
2013-10-18 17:25:52 +0800

    Add support for TCMLS.

--- entity.php
+++ entity.php
@@ -1,5 +1,7 @@
 <?php
 include_once ("./header.php");
+include_once ("./onto_array.php");
+include_once ("./appvars.php");
 /*
   function render_graph($dbc, $name) {
   $query = "select * from graph where subject ='$name'  limit 100";
@@ -68,6 +70,42 @@ function render_graph($dbc, $name, $ontology) {
     }
 }
 
+function render_details($dbc, $name) {
+    $query = "select * from graph where subject ='$name'  limit 100";
+
+    $result = mysqli_query($dbc, $query) or die('Error querying database2.');
+
+    if (mysqli_num_rows($result) != 0) {
+        while ($row = mysqli_fetch_array($result)) {
+            $property = $row[property];
+            $value = $row[value];
+            $id = $row[id];
+            //echo "<p><strong>$property</strong>:&nbsp;$value";
+            echo "<tr><td width='10%'>" . $property . ":</td><td>";
+            render_value($dbc, $value);
+            echo '<a href="' . $_SERVER['PHP_SELF'] . '?name=' . $name . '&delete_triple_id=' . $id . '"><span class="glyphicon glyphicon-remove-circle"></span></a>';
+            echo "</td></tr>";
+        }
+    }
+}
+
+function render_value($dbc, $name) {
+    if (strpos($name, TCMLS_OBJECT) === 0) {
+        $id = str_replace(TCMLS_OBJECT, "", $name);
+        $query = "select * from def where id ='$id'";
+        $result = mysqli_query($dbc, $query) or die('Error querying database1.');
+        if ($row = mysqli_fetch_array($result)) {
+            $name = $row[name];
+            $def = $row[def];
+            echo '<a href="'.$_SERVER['PHP_SELF'].'?id='.$id.'">'. $name .'</a>&nbsp;<em><small>('. $def .')'.'</small></em>';
+        } else {
+            echo $name;
+        }
+    } else {
+        echo $name;
+    }
+}
+
 function get_types($dbc, $name) {
     $query = "select * from graph where subject = '$name' and property = 'a'";
     $types = array();
@@ -79,70 +117,43 @@ function get_types($dbc, $name) {
     return $types;
 }
 
-$formula = array("分类", "组成", "功用", "主治", "用法", "化裁");
-$ontology = array("方剂" => $formula);
-//print_r($ontology["方剂"]); 
-
 if (isset($_GET['delete_triple_id'])) {
     $query = "DELETE FROM graph WHERE id = '" . $_GET['delete_triple_id'] . "'";
     mysqli_query($dbc, $query) or die('Error querying database.');
 }
 
 if (isset($_GET['id'])) {
-
     $id = $_GET['id'];
-    $query = "select name from def where id ='$id'";
+    $query = "select name, def from def where id ='$id'";
     $result = mysqli_query($dbc, $query) or die('Error querying database1.');
     if ($row = mysqli_fetch_array($result)) {
         $name = $row[name];
-        //$def = $row[def];
+        $def = $row[def];
+    } else {
+        render_warning('无相关实体信息！');
     }
 } else if (isset($_GET['name'])) {
     $name = $_GET['name'];
-} else if (isset($_POST['name'])) {
-    //$id = $_POST['id'];
-    $name = $_POST['name'];
-    $property = $_POST['property'];
-    $value = $_POST['value'];
-    $description = $_POST['description'];
-
-    if (($name != '') && ($property != '') && ($value != '')) {
-        //$user_id = $_SESSION[id];
-        $user_id = 2;
-        $property_escape = mysql_escape_string($property);
-        $value_escape = mysql_escape_string($value);
-        $description_escape = mysql_escape_string($description);
-
-        $query = "insert into graph (subject, property, value, description, user_id, date) values ('$name','$property_escape','$value_escape', '$description_escape', '$user_id', NOW()) ";
-
-        mysqli_query($dbc, $query) or die('Error querying database.');
-
-        render_warning('实体信息添加成功！');
+    $query = "select id, def from def where name ='$name'";
+    $result = mysqli_query($dbc, $query) or die('Error querying database1.');
+    if ($row = mysqli_fetch_array($result)) {
+        $id = $row[id];
+        $def = $row[def];
     } else {
-        render_warning('请补全实体信息！');
+        render_warning('无相关实体信息！');
     }
 } else {
     render_warning('无相关实体信息！');
 }
 
-if (isset($name) && $name != '') {
+if (isset($name) && $name != '' && isset($id) && $id != '') {
     ?>
 
 
     <div class="container">
 
         <?php
-        $query = "select * from def where name ='$name'";
-        $result = mysqli_query($dbc, $query) or die('Error querying database2.');
-        if ($row = mysqli_fetch_array($result)) {
-            $def = $row[def];
-        } else {
-            $def = $name;
-        }
-
         $width = 300;
-
-
         echo '<div class="media">';
 
         $image_file = 'img/' . $name . '.jpg';
@@ -157,16 +168,22 @@ if (isset($name) && $name != '') {
         echo '<div class="media-body"  align ="left">';
         echo '<h2>' . $name . '</h2>';
         echo "<p>&nbsp;<a class=\"btn btn-xs btn-primary\" href=\"individual.php\">查看本体信息</a>";
-        echo "&nbsp;<a class=\"btn btn-xs btn-primary\" href=\"individual.php\">添加实体信息</a>";
+        echo "&nbsp;<a class=\"btn btn-xs btn-primary\" href=\"editor.php?name=" . $name . "\">添加实体信息</a>";
         echo "&nbsp;<a class=\"btn btn-xs btn-success\" href=\"individual.php\">返回主页</a></p>";
 
         echo "<table class=\"table\"><tbody>";
+        echo "<tr><td width='10%'>代码:</td><td>" . $id . "</td></tr>";
+
         echo "<tr><td width='10%'>类型:</td><td>" . implode(',', get_types($dbc, $name)) . "</td></tr>";
 
-        echo "<tr><td>定义:</td><td>" . $def;
+        echo "<tr><td>定义/注释:</td><td>" . $def;
         render_info_by_property($dbc, $name, '定义');
         render_info_by_property($dbc, $name, '注释');
         echo "</td></tr>";
+
+
+
+
 
         echo "</tbody></table>";
 
@@ -178,53 +195,21 @@ if (isset($name) && $name != '') {
 
 
         echo '<div class ="container">';
-        if (isset($name)) {
-            render_graph($dbc, $name, $ontology);
-        }
+        render_graph($dbc, $name, $ontology);
+
+        echo '<div class="panel panel-info">';
+        echo '<div class="panel-heading">';
+        echo '<h3 class="panel-title">' . 详细信息 . '</h3>';
+        echo '</div>';
+        echo '<div class="panel-body">';
+
+        echo "<table class=\"table\"><tbody>";
+        render_details($dbc, TCMLS_OBJECT . $id);
+        render_details($dbc, $name);
+        echo "</tbody></table></div></div>";
         ?>
 
 
-        <div class="panel panel-info">
-            <div class="panel-heading">
-                <h3 class="panel-title">请添加实体信息:</h3>
-            </div>
-            <div class="panel-body">
-
-                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="form-horizontal"
-                      enctype="multipart/form-data">
-
-                    <input  type="hidden" id="name" name="name" value = "<?php echo $name; ?>" >
-
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label" for="property">实体属性:</label>
-                        <div class="col-sm-10">
-                            <input class="form-control" type="text" id="property" name="property" value = "<?php if (isset($property)) echo $property; ?>" placeholder="请输入实体的属性名称">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label" for="value">实体取值:</label>
-                        <div class="col-sm-10">
-                            <textarea class="form-control"  id="value" name="value" row="6" placeholder="请输入实体的属性取值"><?php if (isset($value)) echo $value; ?></textarea>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label" for="description">注释:</label>
-                        <div class="col-sm-10">
-                            <textarea class="form-control" id="description" name="description"  placeholder="请输入注释" rows="6"><?php if (isset($description)) echo $description; ?></textarea>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="col-sm-offset-2 col-sm-10">
-                            <input class="btn btn-large btn-primary" type="submit" name="submit" value="提交" />    
-                            <a class="btn btn-large btn-success" href="search.php?keywords=<?php echo $name; ?>">返回首页</a>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
     </div>
 
     </div>
---
+++ onto_array.php
@@ -0,0 +1,5 @@
+<?php
+$formula = array("分类", "组成", "功用", "主治", "用法", "化裁");
+$ontology = array("方剂" => $formula);
+//print_r($ontology["方剂"]); 
+?>
