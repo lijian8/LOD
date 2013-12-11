5a8880040ac9098408ba8a1575d11203d5e8a8f3
asus <asus@asus-PC>
2013-12-11 21:13:45 +0800

    Add TCMLM.

--- db_array.php
+++ db_array.php
@@ -7,10 +7,11 @@ $formula = array("localhost", "root", "yutong", "formula");
 $formula_dic = array("localhost", "root", "yutong", "formula_dic");
 $cases = array("localhost", "root", "yutong", "cases");
 $herbnet = array("localhost", "root", "yutong", "exp");
+$docs = array("localhost", "root", "yutong", "hamster1");
 //$tcmct = array("localhost", "root", "yutong", "tcmls1");
 
-$dbs = array("tcmls" => $tcmls,  "spleen" => $spleen, "herb" => $herb, "herbnet" => $herbnet,  "formula" => $formula, "formula_dic" => $formula_dic, "cases" => $cases);
-$db_labels = array("tcmls" => "TCMLS",  "spleen" => "证候库", "herb" => "中药库", "herbnet" => "Herbnet", "formula" => "方剂库", "formula_dic" => "方剂辞典",  "cases" => "医案库");
+$dbs = array("docs" => $docs, "tcmls" => $tcmls, "spleen" => $spleen, "herb" => $herb, "herbnet" => $herbnet,  "formula" => $formula, "formula_dic" => $formula_dic, "cases" => $cases);
+$db_labels = array("docs" => "文献库", "tcmls" => "TCMLS",  "spleen" => "证候库", "herb" => "中药库", "herbnet" => "Herbnet", "formula" => "方剂库", "formula_dic" => "方剂辞典",  "cases" => "医案库");
 
 
 ?>
--- entity_detail.php
+++ entity_detail.php
@@ -1,23 +1,23 @@
 <?php
-$values = get_property_values($dbc,  $db_name, PREFIX . $id);
-$values = get_reverse_property_values($dbc,  $db_name, PREFIX . $id, $values);
+$values = get_property_values($dbc, $db_name, PREFIX . $id);
+$values = get_reverse_property_values($dbc, $db_name, PREFIX . $id, $values);
 echo '<hr>';
 foreach ($values as $property => $value) {
     ?>
-    
-        <div class="panel panel-info">
-            <div class="panel-heading">
-                <h3 class="panel-title"><?php echo $property; ?></h3>
-            </div>
 
-            <ul class="list-group">
-                <?php
-                foreach ($value as $v) {
-                    echo '<li class="list-group-item">' . $v . '</li>';
-                }
-                ?>
-            </ul>
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <h3 class="panel-title"><?php echo $property; ?></h3>
         </div>
+
+        <ul class="list-group">
+            <?php
+            foreach ($value as $v) {
+                echo '<li class="list-group-item">' . $v . '</li>';
+            }
+            ?>
+        </ul>
+    </div>
 <?php
 }
 ?>
