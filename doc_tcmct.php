c7b15e5ecac92442a39f7565978be6cb2c53f8dc
asus <asus@asus-PC>
2015-04-02 17:34:38 +0800

    Add TCMCT,TCMLS Docs

--- doc_tcmct.php
+++ doc_tcmct.php
@@ -1,35 +1,63 @@
 <?php
 include_once ("./header.php");
 ?>
-<div align="center" class="container">
-    <img width ="100%" src ="img/tcmct_logo.jpg"></img>    
-</div>
-
-
 <div class="container">
-    <br> 
-    <p class="lead"><strong>系统介绍</strong></p>
-    <p>
-        <a href="http://www.cintcm.ac.cn/opencms/opencms/xxyj/zyybzyj/zyyyyx.html" target="_blank">中医药临床术语集（Traditional Chinese Medicine Clinical Terms，TCMCT）</a>
-        是IITCM以中医自身独特理论为核心，借鉴SNOMED CT的概念分轴、语义关联等体例和其在适应信息化发展过程中采用的编码方式，吸收国内已完成的对中医术语标准化研究成果，采用当前成熟的本体论工程和术语知识库构建方法，完成的一部具有中医特色、能够实现与世界水平同步接轨、概念标准、分类合理、编码完善、易于被计算机处理和被临床医生广泛接受的中医药临床术语集。TCMCT中每个术语实例的内容包括：概念词、名称字符类、概念定义、相关概念、概念状态、状态原因等。TCMCT主要收词范围为：中医药领域的国标、行标、全国中医药院校教科书、中医药学主题词表、权威中医药字典、词典以及中医临床病历用词等。
-    </p>
-
-
-    <hr>
-    <p class="lead"><strong>顶层语义网络</strong></p>
-    <p>TCMCT包含一个大型的复杂语义网络，它完整、系统性地表达了中医药临床概念之间的各种语义关系。该语义网络的上层结构包括“术语分类操作性框架” （即语义类型）和“连接词”（即语义关系）。术语分类操作性框架将“临床发现”和“操作/治疗方法”作为两大分类主轴，并设定了“病、证”、“药物器械”、“处方”、“机体形态”、“环境”、“量词”、“连接词”、“特殊概念”等分类轴，从而对所收录的中医药专有术语进行分类。另外，TCMCT使用60个连接词，将具有内在关系的概念两两联结起来，构成形如“概念1+连接词+概念2”的陈述，用以明确描述一个临床事件。
-    </p>
-    <p align="center">
-        <a class="btn btn-lg btn-success" href="sn_profile.php?db_name=tcmct" role="button"><span class="glyphicon glyphicon-search"></span>&nbsp;浏览语义网络</a>
-
-    </p>
-    <br>
-
-
-
-
-</div> <!-- /container -->
-
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <p>
+                <img width ="100%" src ="img/tcmct_logo.jpg"></img>   
+            </p> 
+            <p class="lead"><strong>系统介绍</strong></p>
+            <p>
+                中医药临床术语集（Traditional Chinese Medicine Clinical Terms，TCMCT）
+                是IITCM以中医自身独特理论为核心，借鉴SNOMED CT的概念分轴、语义关联等体例和其在适应信息化发展过程中采用的编码方式，吸收国内已完成的对中医术语标准化研究成果，采用当前成熟的本体论工程和术语知识库构建方法，完成的一部具有中医特色、能够实现与世界水平同步接轨、概念标准、分类合理、编码完善、易于被计算机处理和被临床医生广泛接受的中医药临床术语集。TCMCT中每个术语实例的内容包括：概念词、名称字符类、概念定义、相关概念、概念状态、状态原因等。TCMCT主要收词范围为：中医药领域的国标、行标、全国中医药院校教科书、中医药学主题词表、权威中医药字典、词典以及中医临床病历用词等。
+            </p>
+            <p>
+                <a class="btn btn-default" href="entity.php?id=370495&db_name=tcmct"><span class="glyphicon glyphicon-search">
+                    </span>&nbsp;术语浏览</a>
+                <a class="btn btn-default" href="index.php?db=tcmct"><span class="glyphicon glyphicon-search">
+                    </span>&nbsp;术语检索</a>
+            </p>
+            <hr>
+            <p class="lead"><strong>顶层语义网络</strong></p>
+            <p>TCMCT包含一个大型的复杂语义网络，它完整、系统性地表达了中医药临床概念之间的各种语义关系。该语义网络的上层结构包括“术语分类操作性框架” （即语义类型）和“连接词”（即语义关系）。术语分类操作性框架将“临床发现”和“操作/治疗方法”作为两大分类主轴，并设定了“病、证”、“药物器械”、“处方”、“机体形态”、“环境”、“量词”、“连接词”、“特殊概念”等分类轴，从而对所收录的中医药专有术语进行分类。另外，TCMCT使用60个连接词，将具有内在关系的概念两两联结起来，构成形如“概念1+连接词+概念2”的陈述，用以明确描述一个临床事件。
+            </p>
+            <p>
+                <a class="btn btn-default" href="sn_profile.php?db_name=tcmct" role="button"><span class="glyphicon glyphicon-search"></span>&nbsp;浏览语义网络</a>
+            </p>
+            <hr>
+            <p class="lead"><strong>词库</strong></p>
+            <p>TCMCT已收录10万多个中医临床概念，它们分为12个大类：</p>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <a href="entity.php?id=163&db_name=tcmct">临床发现</a>
+                    <a href="entity.php?id=240&db_name=tcmct">药物/器械</a>
+                    <a href="entity.php?id=244&db_name=tcmct">处方</a>
+                    <a href="entity.php?id=247&db_name=tcmct">机体形态</a>
+                    <a href="entity.php?id=250&db_name=tcmct">环境</a>
+                    <a href="entity.php?id=253&db_name=tcmct">量词</a>
+                    <a href="entity.php?id=260&db_name=tcmct">连接词</a>
+                    <a href="entity.php?id=5836&db_name=tcmct">经验</a>
+                    <a href="entity.php?id=262&db_name=tcmct">中医病证</a>
+                    <a href="entity.php?id=85&db_name=tcmct">操作/治疗方法</a>
+                    <a href="entity.php?id=6113&db_name=tcmct">特殊概念</a>
+                    <a href="entity.php?id=1&db_name=tcmct">原理</a>
+                </div>
+            </div>
+            <hr>
+            <p class="lead"><strong>相关链接</strong></p>
+            <ul>
+                <li>
+                    <p><a href="http://www.cintcm.ac.cn/opencms/opencms/xxyj/zyybzyj/zyyyyx.html" target="_blank">查看中医药临床术语集的介绍</a></p>
+                </li>
+                <li>
+                    <p><a href="http://tcmls.cintcm.com:8888/snomed/tccontroller?pEvent=WELCOME" target="_blank">进入加工系统</a></p>
+                </li>
+            </ul>
+            
+        </div>
+    </div> <!-- /container -->
+</div>
 <?php
 include_once ("./foot.php");
 ?>
--- doc_tcmls.php
+++ doc_tcmls.php
@@ -1,55 +1,98 @@
 <?php
 include_once ("./header.php");
 ?>
-<div align="center" class="container">
-    <img width ="100%" src ="img/tcmls_logo.jpg"></img>    
-</div>
-
-
 <div class="container">
-    <br> 
-    <p class="lead"><strong>系统介绍</strong></p>
-    <p>
-        <a href="http://www.cintcm.ac.cn/opencms/opencms/xxyj/zyybzyj/news_0001.html" target="_blank">中医药学语言系统（Traditional Chinese Medicine Language System，TCMLS）</a>
-   是以中医药学科体系为核心，遵循中医药学语言特点，借鉴语义网络的理念，建立的一个中医药学语言集成系统。它的词库涵盖了中医药学科系统及与中医药学科相关联的生物、植物、化工等自然与人文科学专业词汇。TCMLS的核心组件是一个大型的复杂语义网络，通过将中医药学概念中隐含的各种语义关系全部提取出来，形成关系表，并以此为中心，建立学科术语概念与概念、概念与名词、概念与含义、名词与名词之间的内在联系，形成一个网状的信息表示结构。
-    </p>
-    <hr>
-    <p class="lead"><strong>顶层语义网络</strong></p>
-    <p>语义网络的上层结构包括97种语义类型和55种语义关系，下面分别加以介绍：
-    <ul>
-        <li>语义类型（Semantic Type）：TCMLS制定了97种语义类型，其中39种属于实体类型，而58种属于事件类型。          
-        </li>
-        <li>语义关系（Semantic Relation）：TCMLS制定了55种语义关系，除了代表上下位父子关系的等级关系外，还包括54种相关关系，并且按照物理上相关、空间上相关、影响、时间上相关、概念上相关这五个大类展开。            
-        </li>
-    </ul>
-    <p align="center">
-        <a class="btn btn-lg btn-success" href="sn_profile.php?db_name=tcmls" role="button"><span class="glyphicon glyphicon-search"></span>&nbsp;浏览语义网络</a>
-                
-        <a class="btn btn-lg btn-primary" href="ontologies/tcmls-sn.rdf" role="button"><span class="glyphicon glyphicon-cloud-download"></span>&nbsp;下载OWL本体</a>
-    </p>
-    <br>
-    <p>ISO/TC215健康信息学全体大会于2011年10月19-21日在芝加哥召开，我国代表团全程参加了传统医学任务组（TM TF）会议、语义术语组（WG3）会议、ISO/TC215全体大会。此次会议上，我国代表团提出了标准项目提案“Semantic network framework and coding of traditional Chinese medicine （中医药语义网络框架和代码）”。2012年2月24日，会议投票结果在网上公布：“中医药语义网络框架和代码标准”提案得到批准，作为新的项目立项，由中国、日本、韩国、荷兰和英国参与编制。这是我国中医药信息标准在ISO中首次成功立项，具有突破性的意义。</p>
-    <p align="center">
-        <a class="btn btn-lg btn-success" href="tcmls_sn.php" role="button"><span class="glyphicon glyphicon-book"></span>&nbsp;查看ISO技术规范</a>
-    </p>
-    <hr>
-    <p class="lead"><strong>TCMLS的叙词表</strong></p>    
-    <p>TCMLS在其Semantic Network的框架下，构建了一个面向中医药领域的叙词表。该表以概念为单位对中医药术语资源进行了系统化表达，并建立了概念与概念之间的语义关系。
-    </p>
-     <p align="center">
-        <a class="btn btn-lg btn-success" href="db_profile.php?db_name=tcmls" role="button"><span class="glyphicon glyphicon-search"></span>&nbsp;浏览知识内容</a>
-    </p>
-    <hr>
-    <p class="lead"><strong>总结</strong></p>
-    <p>我所参考了UMLS等语言系统的结构特征，以中医药学科及相关学科知识为主干，设计了中医药学语言系统的结构。TCMLS的整体结构设计原则符合中医药学结构特点，且能满足现有中医药数字化需求。</p>
-    <br>
-    
+    <div class="panel panel-default">   
+        <div class="panel-body">
+            <p>
+                <img width ="100%" src ="img/tcmls_logo.jpg"></img>  
+            </p>
+            <p class="lead"><strong>系统介绍</strong></p>
+            <p>
+                中医药学语言系统（Traditional Chinese Medicine Language System，TCMLS）
+                是以中医药学科体系为核心，遵循中医药学语言特点，借鉴语义网络的理念，建立的一个中医药学语言集成系统。
 
-    <hr>
+                它的词库涵盖了中医药学科系统及与中医药学科相关联的生物、植物、化工等自然与人文科学专业词汇。
+
+                TCMLS的核心组件是一个大型的复杂语义网络，通过将中医药学概念中隐含的各种语义关系全部提取出来，形成关系表，并以此为中心，
+                建立学科术语概念与概念、概念与名词、概念与含义、名词与名词之间的内在联系，形成一个网状的信息表示结构。
+            </p>
+            <p>
+                我所参考了UMLS等语言系统的结构特征，以中医药学科及相关学科知识为主干，设计了中医药学语言系统的结构。
+                TCMLS主要包括语义网络和基础词库两大部分。TCMLS的整体结构设计原则符合中医药学结构特点，且能满足现有中医药数字化需求。
+            </p>
+            <p>
+                <a class="btn btn-default" href="entity.php?id=260000&db_name=tcmls"><span class="glyphicon glyphicon-search">
+                    </span>&nbsp;术语浏览</a>
+                <a class="btn btn-default" href="index.php?db=tcmls"><span class="glyphicon glyphicon-search">
+
+                    </span>&nbsp;术语检索</a>
+
+            </p>
 
 
-</div> <!-- /container -->
+            <hr>
+            <p class="lead"><strong>语义网络</strong></p>
+            <p>语义网络的上层结构包括97种语义类型和55种语义关系，下面分别加以介绍：
+            <ul>
+                <li>语义类型（Semantic Type）：TCMLS制定了97种语义类型，其中39种属于实体类型，而58种属于事件类型。          
+                </li>
+                <li>语义关系（Semantic Relation）：TCMLS制定了55种语义关系，除了代表上下位父子关系的等级关系外，还包括54种相关关系，并且按照物理上相关、空间上相关、影响、时间上相关、概念上相关这五个大类展开。            
+                </li>
+            </ul>
 
+
+            <p>ISO/TC215健康信息学全体大会于2011年10月19-21日在芝加哥召开，我国代表团全程参加了传统医学任务组（TM TF）会议、语义术语组（WG3）会议、ISO/TC215全体大会。此次会议上，我国代表团提出了标准项目提案“Semantic network framework and coding of traditional Chinese medicine （中医药语义网络框架和代码）”。2012年2月24日，会议投票结果在网上公布：“中医药语义网络框架和代码标准”提案得到批准，作为新的项目立项，由中国、日本、韩国、荷兰和英国参与编制。这是我国中医药信息标准在ISO中首次成功立项，具有突破性的意义。</p>
+            <p>
+                <a class="btn btn-default" href="sn_profile.php?db_name=tcmls" role="button"><span class="glyphicon glyphicon-search"></span>&nbsp;浏览语义网络</a>&nbsp;
+                &nbsp;&nbsp;
+                <a class="btn btn-primary" href="tcmls_sn.php" role="button"><span class="glyphicon glyphicon-book"></span>&nbsp;查看ISO技术规范</a>
+            </p>
+            <hr>
+            <p class="lead"><strong>基础词库</strong></p>    
+            <p>TCMLS在其语义网络的框架下，构建了一个面向中医药领域的基础词库。它以概念为单位对中医药术语资源进行了系统化表达，并建立了概念与概念之间的语义关系。
+                TCMLS的基础词库包括如下16个大类：
+            </p>
+
+
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <a href="entity.php?id=163297&db_name=tcmls">自然科学与物理科学</a>&nbsp;&nbsp;
+                    <a href="entity.php?id=163292&db_name=tcmls">地理学</a>&nbsp;&nbsp;
+                    <a href="entity.php?id=324391&db_name=tcmls">治则治法3</a>&nbsp;&nbsp;
+                    <a href="entity.php?id=1018&db_name=tcmls">医学信息学与文献学</a>&nbsp;&nbsp;
+                    <a href="entity.php?id=1&db_name=tcmls">方剂学</a>&nbsp;&nbsp;
+                    <a href="entity.php?id=163298&db_name=tcmls">卫生医疗机构管理</a>&nbsp;&nbsp;
+                    <a href="entity.php?id=66706&db_name=tcmls">病因病机与诊断</a>&nbsp;&nbsp;
+                    <a href="entity.php?id=33645&db_name=tcmls">预防与养生学</a>&nbsp;&nbsp;
+                    <a href="entity.php?id=66834&db_name=tcmls">治则治法</a>&nbsp;&nbsp;
+                    <a href="entity.php?id=276&db_name=tcmls">针灸学</a>&nbsp;&nbsp;
+                    <a href="entity.php?id=1004&db_name=tcmls">药用动植物学</a>&nbsp;&nbsp;
+                    <a href="entity.php?id=1209&db_name=tcmls">中医学说与相关学科</a>&nbsp;&nbsp;
+                    <a href="entity.php?id=1236&db_name=tcmls">疾病</a>&nbsp;&nbsp;
+                    <a href="entity.php?id=163290&db_name=tcmls">人文科学</a>&nbsp;&nbsp;
+                    <a href="entity.php?id=163280&db_name=tcmls">中药化学</a>&nbsp;&nbsp;
+                    <a href="entity.php?id=1108&db_name=tcmls">中药学</a>&nbsp;&nbsp;
+                    <a href="entity.php?id=43&db_name=tcmls">中医基础理论</a> 
+                </div>       
+            </div>   
+            <p>
+                <a class="btn btn-default" href="db_profile.php?db_name=tcmls" role="button"><span class="glyphicon glyphicon-search"></span>&nbsp;浏览概念实体</a>
+            </p>
+            <hr>
+            <p class="lead"><strong>相关链接</strong></p>
+            <ul>
+                <li>
+                    <p><a href="http://www.cintcm.ac.cn/opencms/opencms/xxyj/zyybzyj/news_0001.html" target="_blank">查看中医药学语言系统的介绍</a></p>
+                </li>
+                <li>
+                    <p><a href="http://tcmls.cintcm.com:8888/conceptchoir/tccontroller?pEvent=WELCOME" target="_blank">进入加工系统</a></p>
+                </li>
+            </ul>
+        </div> <!-- /container -->
+
+    </div>
+</div>
 <?php
 include_once ("./foot.php");
 ?>
