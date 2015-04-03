a1f4e7759afb86b65ff8ebbc9510c2afd1249c45
Tong Yu <yutongoracle@hotmail.com>
2015-04-03 13:51:20 +0800

    add support for sub-ontologies

--- entity_detail.php
+++ entity_detail.php
@@ -1,10 +1,8 @@
 <?php
 $values = get_property_values($dbc, $db_name, PREFIX . $id);
-
 echo '<hr>';
-
 include("pv_panel.php");
-
 $values = get_reverse_property_values($dbc, $db_name, PREFIX . $id);
 include("pv_panel.php");
+
 ?>
--- pv_panel.php
+++ pv_panel.php
@@ -1,41 +1,19 @@
 <?php
 foreach ($values as $property => $value) {
-    
-    //if (count($values) < 100) {
-        ?>
-
-        <div class="panel panel-info">
-            <div class="panel-heading">
-                <h3 class="panel-title"><?php echo $property; ?></h3>
-            </div>
-
-            <ul class="list-group">
-                <?php
-                foreach ($value as $v) {
-                    echo '<li class="list-group-item">' . $v . '</li>';
-                }
-                ?>
-            </ul>
-        </div>
-       <?php
-}
-    //} else {
     ?>
-<!--
-        <div class="panel panel-info">
-            <div class="panel-heading">
-                <h3 class="panel-title"> //echo $property; </h3>
-            </div>
-
-            <ul class="nav nav-pills">
-                ?php
-                //foreach ($value as $v) {
-                  //  echo '<li>' . $v . '</li>';
-                //}
-                ?>
-            </ul>
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <h3 class="panel-title"><?php echo $property; ?></h3>
         </div>
-       
-   // }
+
+        <ul class="list-group">
+            <?php
+            foreach ($value as $v) {
+                echo '<li class="list-group-item">' . $v . '</li>';
+            }
+            ?>
+        </ul>
+    </div>
+    <?php
 }
 ?>
